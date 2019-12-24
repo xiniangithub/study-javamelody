aad30260ee47918b3ada1fa715aaf3c6866f5a63
Admin <2779207210@qq.com>
2019-12-24 22:55:45 +0800

    监控SQL配置

--- src/main/java/com/wez/ctrl/IndexController.java
+++ src/main/java/com/wez/ctrl/IndexController.java
@@ -1,16 +1,27 @@
 package com.wez.ctrl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/index")
 public class IndexController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @RequestMapping(value = "/toIndex", method = RequestMethod.GET)
-    public String toIndex(String param) {
-        return param;
+    public List<Map<String, Object>> toIndex(String param) {
+        String sql = "select * from dual";
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+        return maps;
     }
 
 }
